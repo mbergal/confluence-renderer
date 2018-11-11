339ad1a32ad38c29e3aa96c450e501ed967de8d0
mbergal <misha.bergal@gmail.com>
2018-11-10 18:14:28 -0600

    Fix run-renderer.cmd

--- run-renderer.cmd
+++ run-renderer.cmd
@@ -1,2 +1,2 @@
 start browser-sync start --server --files "*.html"
-start E:\Users\mbergal\Projects\confluence-renderer\ConfluenceRenderer\bin\Debug\ConfluenceRenderer.exe %*
+start %~dp0ConfluenceRenderer\bin\Debug\ConfluenceRenderer.exe %*
